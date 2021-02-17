e2f164cce8a597bfe6f56e9dc466c02326666347
vitalities <vitalik.mokiychuk@gmail.com>
2021-02-17 15:59:08 +0200

    update

--- script.js
+++ script.js
@@ -1,42 +1,45 @@
+$( document ).ready(function() {
+  $( ".portfolio-photo-item" ).each(function() {
+    const thisBlock = $(this);
+    const thisImg = $(this).find('img');
+    var portfolioBlockWidth = $(this).width()
+    var portfolioBlockHeight = $(this).height()
+    var portfolioImgWidth = $(this).find('img').width()
+    var portfolioImgHeight = $(this).find('img').height()
+    var imgRes = portfolioImgWidth / portfolioImgHeight;
+    var blockRes = portfolioBlockWidth / portfolioBlockHeight;
 
-$( ".portfolio-photo-item" ).each(function() {
-  const thisBlock = $(this);
-  const thisImg = $(this).find('img');
-  var portfolioBlockWidth = $(this).width()
-  var portfolioBlockHeight = $(this).height()
-  var portfolioImgWidth = $(this).find('img').width()
-  var portfolioImgHeight = $(this).find('img').height()
-  var imgRes = portfolioImgWidth / portfolioImgHeight;
-  var blockRes = portfolioBlockWidth / portfolioBlockHeight;
+    if (imgRes >= blockRes) {
+      var thisDiv = $(this);
 
-  if (imgRes >= blockRes) {
-    var thisDiv = $(this);
+      $(this).find('img').height(portfolioBlockHeight);
+      $(this).find('img').width('auto')
+      setTimeout(function () {
+        var portfolioImgWidth = thisDiv.find('img').width();
+        var imblockd = portfolioImgWidth - portfolioBlockWidth;
+        imblockd /= 2;
+        var imblockh = portfolioBlockHeight - portfolioImgHeight;
+        imblockh /= 1.345;
+        thisDiv.find('img').css('right', imblockd);
+        thisDiv.find('img').css('bottom', imblockh);
+      }, 900);
+    }else {
+      var thisDiv = $(this);
+      $(this).find('img').width(portfolioBlockWidth);
+      $(this).find('img').height('auto');
+      setTimeout(function () {
+        var portfolioImgWidth = thisDiv.find('img').width();
+        var imblockh = portfolioBlockHeight - portfolioImgHeight;
+        imblockh /= 1.05;
+        thisDiv.find('img').css('bottom', imblockh);
+      }, 900);
+    }
 
-    $(this).find('img').height(portfolioBlockHeight);
-    $(this).find('img').width('auto')
-    setTimeout(function () {
-      var portfolioImgWidth = thisDiv.find('img').width();
-      var imblockd = portfolioImgWidth - portfolioBlockWidth;
-      imblockd /= 2;
-      var imblockh = portfolioBlockHeight - portfolioImgHeight;
-      imblockh /= 1.345;
-      thisDiv.find('img').css('right', imblockd);
-      thisDiv.find('img').css('bottom', imblockh);
-    }, 800);
-  }else {
-    var thisDiv = $(this);
-    $(this).find('img').width(portfolioBlockWidth);
-    $(this).find('img').height('auto');
-    setTimeout(function () {
-      var portfolioImgWidth = thisDiv.find('img').width();
-      var imblockh = portfolioBlockHeight - portfolioImgHeight;
-      imblockh /= 1.05;
-      thisDiv.find('img').css('bottom', imblockh);
-    }, 800);
-  }
+  });
+
+  setTimeout(function () {
+    $('.portfolio-item').fadeTo(100, 1);
+  }, 900);
+
 
 });
-
-setTimeout(function () {
-  $('.portfolio-item').fadeTo(100, 1);
-}, 800);
