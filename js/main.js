20b5e9e9df7b8c42574912f829cc53d162be0e73
vitalities <vitalik.mokiychuk@gmail.com>
2022-11-04 12:41:35 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+// xmlhttp = new XMLHttpRequest();
+//
+// url = '127.0.0.1'
+//
+// xmlhttp.open("POST", url)
+// xmlhttp.setRequestHeader('Access-Control-Allow-Origin', '*')
+// xmlhttp.setRequestHeader('Access-Control-Allow-Headers', '')
+
 
 // Copyright
 $('#copyright').text(new Date().getFullYear()+'. Designed by Vitaliy Mokiychuk')
